372365b7d5500e909bc920134f98dd98882c6941
sohag838 <95273824+sohag838@users.noreply.github.com>
2023-03-27 23:37:24 +0600

    Update some content

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "../../images/Logo.svg";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <nav className="header">
+      <img src={logo} alt="" />
+      <div className="nav-content">
+        <a href="/shop">Shop</a>
+        <a href="/order">Orders</a>
+        <a href="/inventory">Inventory</a>
+        <a href="/login">Login</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
