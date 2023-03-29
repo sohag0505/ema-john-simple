ec0bb7fad80b36e2b863b0ffe4bad05dddea1c96
sohag838 <95273824+sohag838@users.noreply.github.com>
2023-03-29 17:39:02 +0600

    show data

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,9 +7,12 @@ const Cart = (props) => {
 
   let total = 0;
   let totalShipping = 0;
+  let quantity = 0;
   for (const product of cart) {
-    total = total + product.price;
+    product.quantity = product.quantity || 1;
+    total = total + product.price * product.quantity;
     totalShipping = totalShipping + product.shipping;
+    quantity = quantity + product.quantity;
   }
   const tax = (total * 7) / 100;
 
@@ -17,7 +20,7 @@ const Cart = (props) => {
   return (
     <div className="cart">
       <h2>Order summary</h2>
-      <h4>Selected iteams: {cart.length}</h4>
+      <h4>Selected iteams: {quantity}</h4>
       <p>Total Price: ${total}</p>
       <p>Total Shipping: ${totalShipping}</p>
       <p>Tax: ${tax.toFixed(2)}</p>
